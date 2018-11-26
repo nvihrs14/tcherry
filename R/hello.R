23a2cc9443ec061900451aec195cf98e4f661107
nvihrs14 <nvihrs14@student.aau.dk>
2018-11-26 15:22:07 +0100

    leg

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,20 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+# Hello, world!  This is an example function named 'hello' which prints
+# 'Hello, world!'.  You can learn more about package authoring with RStudio
+# at: http://r-pkgs.had.co.nz/ Some useful keyboard shortcuts for package
+# authoring: Build and Reload Package: 'Ctrl + Shift + B' Check Package:
+# 'Ctrl + Shift + E' Test Package: 'Ctrl + Shift + T'
 
-hello <- function() {
+hello <- function(){
   print("Hello, world!")
 }
+
+#' Add two numbers
+#'
+#' @param x,y numeric numbers
+#' @return The sum of \code{x} and \code{y}
+#' @examples
+#' sum(2,3)
+sum <- function(x,y){
+  x+y
+}
+
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(tcherry)
+
+test_check("tcherry")
