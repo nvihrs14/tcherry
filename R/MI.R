6f86961848d6adcf27cb05a1cce0befcb79a491e
nvihrs14 <nvihrs14@student.aau.dk>
2018-11-27 15:34:58 +0100

    layout fixes

--- R/MI.R
+++ R/MI.R
@@ -1,48 +1,51 @@
 
 
-#' Calculate mutual information
-#' @description Calculate mutual information for two or three            categorical variables.
-#'
-#' @param x,y,z Vectors of class character or factor.
-#' @param smooth A single numeric non-negative value. See 'details'      below.
-#' @param log_base The base of the logarithmic function to be used.
-#' @details The \code{smooth} argument is added to all cell counts in     tables before normalisation. This is meant as a way to avoid zero     counts, leading to zero probabilities.
-#'
-#' The mutual information for two variables is calculated by the         formula \deqn{MI(x, y) = \sum P(x, y) log(P(x, y) / (P(x)P(y)))}     where the sum is over alle possible values of x and y.
-#'
-#' The mutual information for three variables is calculated by the      formula \deqn{MI(x, y, z) = \sum P(x, y, z) log(P(x, y, z) / (P      (x)P(y)P(z)))} where the sum is over all possible values of x, y     and z.
-#' @return The mutual information given by a single numeric value.
-#' @author
-#' Katrine Kirkeby, \email{enir_tak@@hotmail.com}
-#'
-#' Maria Knudsen, \email{mariaknudsen@@hotmail.dk}
-#'
-#' Ninna Vihrs, \email{ninnavihrs@@hotmail.dk}
-#' @importFrom Rdpack reprompt
-#' @references
-#' \insertRef{TCJT}{tcherry}
-#'
-#' \insertRef{EKTS}{tcherry}
-#' @examples
-#'
-#' x <- c('hund','kat','kat','kat','hund','hund','hund','hund','hund')
-#' y <- c('a','b','c','a','c','b','a','a','b')
-#' MI2(x, y)
-#' MI2(x, y, smooth = 1)
-#' MI2(x, y, log_base = exp(1))
-#' @export
+#" Calculate mutual information
+#" @description Calculate mutual information for two or three            categorical variables.
+#"
+#" @param x,y,z Vectors of class character or factor.
+#" @param smooth A single numeric non-negative value. See "details"      below.
+#" @param log_base The base of the logarithmic function to be used.
+#" @details The \code{smooth} argument is added to all cell counts in     tables before normalisation. This is meant as a way to avoid zero     counts, leading to zero probabilities.
+#"
+#" The mutual information for two variables is calculated by the         formula \deqn{MI(x, y) = \sum P(x, y) log(P(x, y) / (P(x)P(y)))}     where the sum is over alle possible values of x and y.
+#"
+#" The mutual information for three variables is calculated by the      formula \deqn{MI(x, y, z) = \sum P(x, y, z) log(P(x, y, z) / (P      (x)P(y)P(z)))} where the sum is over all possible values of x, y     and z.
+#" @return The mutual information given by a single numeric value.
+#" @author
+#" Katrine Kirkeby, \email{enir_tak@@hotmail.com}
+#"
+#" Maria Knudsen, \email{mariaknudsen@@hotmail.dk}
+#"
+#" Ninna Vihrs, \email{ninnavihrs@@hotmail.dk}
+#" @importFrom Rdpack reprompt
+#" @references
+#" \insertRef{TCJT}{tcherry}
+#"
+#" \insertRef{EKTS}{tcherry}
+#" @examples
+#"
+#" x <- c("hund","kat","kat","kat","hund","hund","hund","hund","hund")
+#" y <- c("a","b","c","a","c","b","a","a","b")
+#" MI2(x, y)
+#" MI2(x, y, smooth = 1)
+#" MI2(x, y, log_base = exp(1))
+#" @export
 MI2 <- function(x, y, smooth = 0, log_base = 2){
-  if (! all(sapply(list(x, y), function(x){is.character(x) | is.factor(x)}))){
-    stop('x and y must be either characters or factors')
+  if (! all(sapply(list(x, y), function(x){
+    is.character(x) | is.factor(x)
+    }
+    ))){
+    stop("x and y must be either characters or factors")
   }
   if (length(smooth) > 1){
-    stop('smooth must be a single non-negative value')
+    stop("smooth must be a single non-negative value")
   }
   else if (!is.numeric(smooth)) {
-    stop('smooth must be numeric')
+    stop("smooth must be numeric")
   }
   else if (smooth < 0){
-    stop('smooth must be a non-negative numeric value')
+    stop("smooth must be a non-negative numeric value")
   }
 
   tab_x <- table(x) + smooth
@@ -50,34 +53,38 @@ MI2 <- function(x, y, smooth = 0, log_base = 2){
   tab_xy <- table(x, y) + smooth
 
   if (0 %in% c(tab_x, tab_xy, tab_y)){
-    stop('Some probabilities are zero and therefore MI cannot be calculated. Consider using the smooth argument.')
+    stop("Some probabilities are zero and therefore MI cannot be calculated.
+         Consider using the smooth argument.")
   }
 
   prop_x <- c(tab_x / sum(tab_x))
   prop_y <- c(tab_y / sum(tab_y))
   prop_xy <- tab_xy / sum(tab_xy)
 
-  frac_prop_MI <- sweep(prop_xy, 1, prop_x, FUN='/')
-  frac_prop_MI <- sweep(frac_prop_MI, 2, prop_y, FUN='/')
+  frac_prop_MI <- sweep(prop_xy, 1, prop_x, FUN = "/")
+  frac_prop_MI <- sweep(frac_prop_MI, 2, prop_y, FUN = "/")
 
   MI <- sum(prop_xy * log(frac_prop_MI, base = log_base))
 
   return(MI)
 }
 
-#' @rdname MI2
+#" @rdname MI2
 MI3 <- function(x, y, z, smooth = 0, log_base = 2){
-  if (! all(sapply(list(x, y, z), function(x){is.character(x) | is.factor(x)}))){
-    stop('x, y and z must be either characters or factors')
+  if (! all(sapply(list(x, y, z), function(x){
+    is.character(x) | is.factor(x)
+    }
+    ))){
+    stop("x, y and z must be either characters or factors")
   }
   if (length(smooth) > 1){
-    stop('smooth must be a single non-negative value')
+    stop("smooth must be a single non-negative value")
   }
   else if (!is.numeric(smooth)) {
-    stop('smooth must be numeric')
+    stop("smooth must be numeric")
   }
   else if (smooth < 0){
-    stop('smooth must be a non-negative numeric value')
+    stop("smooth must be a non-negative numeric value")
   }
 
   tab_x <- table(x) + smooth
@@ -86,7 +93,8 @@ MI3 <- function(x, y, z, smooth = 0, log_base = 2){
   tab_xyz <- table(x, y, z) + smooth
 
   if (0 %in% c(tab_x, tab_y, tab_z, tab_xyz)){
-    stop('Some probabilities are zero and therefore MI cannot be calculated. Consider using the smooth argument.')
+    stop("Some probabilities are zero and therefore MI cannot be calculated.
+         Consider using the smooth argument.")
   }
 
   prop_x <- c(tab_x / sum(tab_x))
@@ -94,13 +102,11 @@ MI3 <- function(x, y, z, smooth = 0, log_base = 2){
   prop_z <- c(tab_z / sum(tab_z))
   prop_xyz <- tab_xyz / sum(tab_xyz)
 
-  frac_prop_MI <- sweep(prop_xyz, 1, prop_x, FUN='/')
-  frac_prop_MI <- sweep(frac_prop_MI, 2, prop_y, FUN='/')
-  frac_prop_MI <- sweep(frac_prop_MI, 3, prop_z, FUN='/')
+  frac_prop_MI <- sweep(prop_xyz, 1, prop_x, FUN = "/")
+  frac_prop_MI <- sweep(frac_prop_MI, 2, prop_y, FUN = "/")
+  frac_prop_MI <- sweep(frac_prop_MI, 3, prop_z, FUN = "/")
 
   MI <- sum(prop_xyz * log(frac_prop_MI, base = log_base))
 
   return(MI)
 }
-
-
--- tests/testthat/testMI.R
+++ tests/testthat/testMI.R
@@ -1,54 +1,66 @@
-context('MI2')
+context("MI2")
 library(tcherry)
 
-test_that('Input is correctly specified',{
-  expect_error(MI2(c(1.5,3.6),c('1','2')),'x and y must be either characters or factors')
-  expect_error(MI2(c('1','2'),c('1','2'),c(1,2)),'smooth must be a single non-negative value')
-  expect_error(MI2(c('1','2'),c('1','2'),'C'),'smooth must be numeric')
-  expect_error(MI2(c('1','2'),c('1','2'),-1),'smooth must be a non-negative numeric value')
+test_that("Input is correctly specified", {
+  expect_error(MI2(c(1.5, 3.6), c("1", "2")),
+               "x and y must be either characters or factors")
+  expect_error(MI2(c("1", "2"), c("1", "2"), c(1, 2)),
+               "smooth must be a single non-negative value")
+  expect_error(MI2(c("1", "2"), c("1", "2"), "C"),
+               "smooth must be numeric")
+  expect_error(MI2(c("1", "2"), c("1", "2"), -1),
+               "smooth must be a non-negative numeric value")
 })
 
 
-x <- c('hund','kat','kat','kat','hund','hund','hund','hund','hund')
-y <- c('a','b','c','a','c','b','a','a','b')
+x <- c("hund", "kat", "kat", "kat", "hund", "hund",
+       "hund", "hund", "hund")
+y <- c("a", "b", "c", "a", "c", "b", "a", "a", "b")
 
 MI_result <- 0.02940695
 
-test_that('MI is correct',{
-  expect_equal(MI2(x,y),MI_result)
+test_that("MI is correct", {
+  expect_equal(MI2(x, y), MI_result)
 })
 
-x1 <- c('1','2','3')
-y1 <- c('a','b','b')
+x1 <- c("1", "2", "3")
+y1 <- c("a", "b", "b")
 
-test_that('an error occur when zero probabilities', {
-  expect_error(MI2(x1, y1), 'Some probabilities are zero and therefore MI cannot be calculated. Consider using the smooth argument.')
+test_that("an error occur when zero probabilities", {
+  expect_error(MI2(x1, y1), "Some probabilities are zero and therefore MI cannot be calculated.
+         Consider using the smooth argument.")
 })
 
-context('MI3')
+context("MI3")
 
-test_that('Input is correctly specified',{
-  expect_error(MI3(c(1.5, 3.6), c('1', '2'), c('a', 'b')),'x, y and z must be either characters or factors')
-  expect_error(MI3(c('1', '2'), c('1', '2'), c('1', '2'), c(1, 2)),'smooth must be a single non-negative value')
-  expect_error(MI3(c('1', '2'), c('1', '2'), c('1', '2'), 'C'),'smooth must be numeric')
-  expect_error(MI3(c('1', '2'), c('1', '2'), c('1', '2'), -1),'smooth must be a non-negative numeric value')
+test_that("Input is correctly specified", {
+  expect_error(MI3(c(1.5, 3.6), c("1", "2"), c("a", "b")),
+               "x, y and z must be either characters or factors")
+  expect_error(MI3(c("1", "2"), c("1", "2"), c("1", "2"), c(1, 2)),
+               "smooth must be a single non-negative value")
+  expect_error(MI3(c("1", "2"), c("1", "2"), c("1", "2"), "C"),
+               "smooth must be numeric")
+  expect_error(MI3(c("1", "2"), c("1", "2"), c("1", "2"), -1),
+               "smooth must be a non-negative numeric value")
 })
 
-x1 <- c('1', '2', '3')
-y1 <- c('a', 'b', 'b')
-z1 <- c('hej', 'goddag', 'hej')
+x1 <- c("1", "2", "3")
+y1 <- c("a", "b", "b")
+z1 <- c("hej", "goddag", "hej")
 
-test_that('an error occur when zero probabilities', {
-  expect_error(MI3(x1, y1, z1), 'Some probabilities are zero and therefore MI cannot be calculated. Consider using the smooth argument.')
+test_that("an error occur when zero probabilities", {
+  expect_error(MI3(x1, y1, z1), "Some probabilities are zero and therefore MI cannot be calculated.
+         Consider using the smooth argument.")
 })
 
-x <- c('hund','kat','kat','kat','hund','hund','hund','hund','hund')
-y <- c('a','b','c','a','c','b','a','a','b')
-z <- c('1', '2', '1', '1', '2', '1', '2', '2', '2')
+x <- c("hund", "kat", "kat", "kat", "hund", "hund",
+       "hund", "hund", "hund")
+y <- c("a", "b", "c", "a", "c", "b", "a", "a", "b")
+z <- c("1", "2", "1", "1", "2", "1", "2", "2", "2")
 smooth <- 0.1
 
 MI_result_3 <- 0.3172042
 
-test_that('MI is calculated correctly', {
+test_that("MI is calculated correctly", {
   expect_equal(MI3(x, y, z, smooth = smooth), MI_result_3)
 })
