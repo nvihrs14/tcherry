c6c4dcf33f1ebd118def4a043d881c35a1b25f85
nvihrs14 <nvihrs14@student.aau.dk>
2019-03-06 14:54:43 +0100

    documentation and test for tcherry_step

--- R/k_tcherry_step.R
+++ R/k_tcherry_step.R
@@ -1,3 +1,84 @@
+#' Determine a k'th order t-cherry tree from data
+#'
+#' @description Determine the structure of a k'th order t-cherry tree
+#' from data based on a greedy stepwise approach.
+#'
+#' @param data The data the tree structure should be based on.
+#' @param k The order of the t-cherry tree.
+#' @param ... Additional arguments passed to \code{MIk}.
+#'
+#' @details Notice that for \eqn{k=3} it is the same as using
+#' \code{tcherry_step} and for \eqn{k=2} it is the same as using
+#' \code{ChowLiu}.
+#'
+#' The algorithm for constructing the t-cherry tree from
+#' data is based on an atempt to minimize the Kullback-Leibler
+#' divergence. The first cherry is chosen as the k variables with
+#' highest mutual information. This is the preliminary t-cherry
+#' tree. Then all possible new cherries are added stepwise to this
+#' tree and the weight \deqn{\sum MI(Clique) - \sum MI(Separator)} is
+#' calculated.
+#' The first sum is over the cliques and the second over the
+#' separators of the junction tree of the preliminary t-cherry tree.
+#' The one with the highest weight is chosen as the new preliminary
+#' t-cherry tree, and the procedure is repeated untill all variables
+#' has been added.
+#'
+#' @return A list containing the following components:
+#' \itemize{
+#' \item \code{adj_matrix} The adjacency matrix for the k'th order
+#' t-cherry tree.
+#' \item \code{weight} The weight of the final k'th order t-cherry tree.
+#' \item \code{cliques} A list containing the cliques (cherries) of
+#'  the k'th order t-cherry tree.
+#' \item \code{separators} A list containing the separators of a
+#' junction tree for the k'th order t-cherry tree.
+#' }
+#'
+#' @author
+#' Katrine Kirkeby, \email{enir_tak@@hotmail.com}
+#'
+#' Maria Knudsen, \email{mariaknudsen@@hotmail.dk}
+#'
+#' Ninna Vihrs, \email{ninnavihrs@@hotmail.dk}
+#'
+#' @seealso \code{\link{MIk}} for mutual
+#' information of k variables.
+#'
+#' @examples
+#' set.seed(43)
+#' var1 <- c(sample(c(1, 2), 100, replace = TRUE))
+#' var2 <- var1 + c(sample(c(1, 2), 100, replace = TRUE))
+#' var3 <- var1 + c(sample(c(0, 1), 100, replace = TRUE,
+#'                         prob = c(0.9, 0.1)))
+#' var4 <- c(sample(c(1, 2), 100, replace = TRUE))
+#' var5 <- var2 + var3
+#' var6 <- var1 - var4 + c(sample(c(1, 2), 100, replace = TRUE))
+#' var7 <- c(sample(c(1, 2), 100, replace = TRUE))
+#'
+#' data <- data.frame("var1" = as.character(var1),
+#'                    "var2" = as.character(var2),
+#'                    "var3" = as.character(var3),
+#'                    "var4" = as.character(var4),
+#'                    "var5" = as.character(var5),
+#'                    "var6" = as.character(var6),
+#'                    "var7" = as.character(var7))
+#'
+#' # smooth used in MIk
+#' (tch <- k_tcherry_step(data, 3, smooth = 0.1))
+#'
+#' # For plotting
+#' library(gRbase)
+#' library(Rgraphviz)
+#' tcherry_tree <- as(tch$adj_matrix, "graphNEL")
+#' plot(tcherry_tree)
+#'
+#' # For probability propagation
+#' library(gRain)
+#' model <- grain(tcherry_tree, data = data, smooth = 0.1)
+#' querygrain(model)
+#' @export
+
 k_tcherry_step <- function(data, k, ...){
   if (! (is.data.frame(data) | is.matrix(data))) {
     stop("data must be a data frame or a matrix.")
@@ -8,6 +89,10 @@ k_tcherry_step <- function(data, k, ...){
   }
   ))){
     stop("Some columns are not characters or factors.")
+  }
+
+  if (length(k) != 1){
+    stop("k must be a single positive integer.")
   }
 
   if (k %% 1 != 0 | k <= 1){
@@ -40,15 +125,15 @@ k_tcherry_step <- function(data, k, ...){
   adj_matrix[tcherry_nodes, tcherry_nodes] <- 1
   diag(adj_matrix[tcherry_nodes, tcherry_nodes]) <- 0
 
-  score <- max(MI)
+  weight <- max(MI)
 
   idx_list <- 1
 
   while (length(tcherry_nodes) != n_var) {
-    score_next_step <- rep(NA, length(tcherry_hyperedges) *
+    weight_next_step <- rep(NA, length(tcherry_hyperedges) *
                              length(nodes_remaining))
     new_cliques_list <- new_seps_list <- new_var_list <-
-      as.list(score_next_step)
+      as.list(weight_next_step)
     idx <- 1
     for (i in 1:length(tcherry_hyperedges)){
       for (var in nodes_remaining){
@@ -58,7 +143,7 @@ k_tcherry_step <- function(data, k, ...){
         MI_sep <- MIk(new_sep, data, ...)
         MI_clique <- MIk(new_clique, data, ...)
 
-        score_next_step[idx] <- score + MI_clique - MI_sep
+        weight_next_step[idx] <- weight + MI_clique - MI_sep
         new_cliques_list[[idx]] <- new_clique
         new_seps_list[[idx]] <- new_sep
         new_var_list[[idx]] <- var
@@ -66,12 +151,12 @@ k_tcherry_step <- function(data, k, ...){
       }
     }
 
-    idx_max_score <- which.max(score_next_step)
-    score <- score_next_step[idx_max_score]
+    idx_max_weight <- which.max(weight_next_step)
+    weight <- weight_next_step[idx_max_weight]
 
-    new_clique <- new_cliques_list[[idx_max_score]]
-    new_sep <- new_seps_list[[idx_max_score]]
-    new_var <- new_var_list[[idx_max_score]]
+    new_clique <- new_cliques_list[[idx_max_weight]]
+    new_sep <- new_seps_list[[idx_max_weight]]
+    new_var <- new_var_list[[idx_max_weight]]
 
     tcherry_nodes <- c(tcherry_nodes, new_var)
     nodes_remaining <- setdiff(nodes, tcherry_nodes)
@@ -93,7 +178,7 @@ k_tcherry_step <- function(data, k, ...){
   }
 
   return(list("adj_matrix" = adj_matrix,
-              "score" = score,
+              "weight" = weight,
               "cliques" = cliques,
               "separators" = separators))
 }
---
+++ tests/testthat/testk_tcherry_step.R
@@ -0,0 +1,59 @@
+context("k_tcherry_step")
+library(tcherry)
+
+set.seed(43)
+var1 <- c(sample(c(1, 2), 100, replace = TRUE))
+var2 <- var1 + c(sample(c(1, 2), 100, replace = TRUE))
+var3 <- var1 + c(sample(c(0, 1), 100, replace = TRUE,
+                        prob = c(0.9, 0.1)))
+var4 <- c(sample(c(1, 2), 100, replace = TRUE))
+var5 <- var2 + var3
+var6 <- var1 - var4 + c(sample(c(1, 2), 100, replace = TRUE))
+var7 <- c(sample(c(1, 2), 100, replace = TRUE))
+
+
+data <- data.frame("var1" = as.character(var1),
+                   "var2" = as.character(var2),
+                   "var3" = as.character(var3),
+                   "var4" = as.character(var4),
+                   "var5" = as.character(var5),
+                   "var6" = as.character(var6),
+                   "var7" = as.character(var7))
+
+data_matrix <- as.matrix(data)
+
+data_numeric <- data
+data_numeric[, 3] <- as.numeric(data_numeric[, 3])
+
+vec <- rep(1:2, 5)
+
+test_that("error messages work", {
+  expect_error(k_tcherry_step(data_numeric, 3, smooth = 0.001),
+               "Some columns are not characters or factors.")
+  expect_error(k_tcherry_step(vec, k, smooth = 0.001),
+               "data must be a data frame or a matrix.")
+  expect_error(k_tcherry_step(data, vec, smooth = 0.001),
+               "k must be a single positive integer.")
+  expect_error(k_tcherry_step(data, 1.1, smooth = 0.001),
+               "k must be a positive integer and at least 2.")
+  expect_error(k_tcherry_step(data, -1, smooth = 0.001),
+               "k must be a positive integer and at least 2.")
+  expect_error(k_tcherry_step(data, 1, smooth = 0.001),
+               "k must be a positive integer and at least 2.")
+})
+
+tch2 <- k_tcherry_step(data, 2, smooth = 0.1)
+tch3 <- k_tcherry_step(data, 3, smooth = 0.1)
+
+CL <- ChowLiu(data, CPTs = FALSE, smooth = 0.1)
+tchstep <- tcherry_step(data, smooth = 0.1)
+
+test_that("results are correct", {
+  expect_equal(tch2$adj_matrix, CL$skeleton_adj)
+  expect_equal(tch3$adj_matrix, tchstep$adj_matrix)
+  expect_equal(tch3$weight, tchstep$weight)
+  expect_true(compare::compare(tch3$cliques, tchstep$cliques,
+                               ignoreOrder = TRUE)$result)
+  expect_true(compare::compare(tch3$separators, tchstep$separators,
+                               ignoreOrder = TRUE)$result)
+})
